a2985386461c756bc5fb15df27cee34fdcbcf2cc
AEManov20 <AEManov20@codingburgas.bg>
2023-01-08 12:49:46 +0200

    Improve input system in stat-test

--- stat-test/src/main.rs
+++ stat-test/src/main.rs
@@ -1,14 +1,16 @@
 use stat_info::StatisticalInfo;
+use std::io::BufRead;
 
 fn main() {
-    let mut inp = String::new();
-    std::io::stdin().read_line(&mut inp).expect("please input something");
-    let mut inp: Vec<i32> = inp
-        .trim()
-        .split(" ")
-        .map(|part| part.parse().unwrap())
-        .collect();
-    
-    inp.sort();
-    println!("{:?}\n{:#?}", inp, StatisticalInfo::new(&inp));
+    let mut lines = std::io::stdin().lock().lines();
+    while let Some(Ok(inp)) = lines.next()
+    {
+        let mut inp: Vec<i32> = inp
+            .trim()
+            .split(" ")
+            .map(|part| part.parse().expect("invalid number"))
+            .collect();
+        inp.sort();
+        println!("{:?}\n{:?}\n", inp, StatisticalInfo::new(&inp));
+    }
 }
