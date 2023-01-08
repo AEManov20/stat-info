89a17a033d1b96090d4189b4b0e37cbf45a4590a
AEManov20 <AEManov20@codingburgas.bg>
2023-01-08 13:05:10 +0200

    Add timings in debug-mode

--- stat-test/src/main.rs
+++ stat-test/src/main.rs
@@ -1,16 +1,25 @@
 use stat_info::StatisticalInfo;
-use std::io::BufRead;
+use std::{io::BufRead, time};
+
 
 fn main() {
+    
     let mut lines = std::io::stdin().lock().lines();
     while let Some(Ok(inp)) = lines.next()
     {
+        let now = time::Instant::now();
+
         let mut inp: Vec<i32> = inp
             .trim()
             .split(" ")
             .map(|part| part.parse().expect("invalid number"))
             .collect();
         inp.sort();
-        println!("{:?}\n{:?}\n", inp, StatisticalInfo::new(&inp));
+        println!("{:?}\n{:?}", inp, StatisticalInfo::new(&inp));
+
+        if cfg!(debug_assertions) {
+            println!("{:?}\n", time::Instant::now().duration_since(now));
+        } else { println!(); }
     }
+
 }
